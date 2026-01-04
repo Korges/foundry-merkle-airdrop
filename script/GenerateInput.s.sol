37274db341d4276b858b60643beb53dfb85eb2a9
korges <korgesik@gmail.com>
2026-01-04 10:58:21 +0100

    Add zksync support

--- script/GenerateInput.s.sol
+++ script/GenerateInput.s.sol
@@ -2,7 +2,6 @@
 pragma solidity ^0.8.30;
 
 import { Script } from "forge-std/Script.sol";
-import { stdJson } from "forge-std/StdJson.sol";
 import { console } from "forge-std/console.sol";
 
 // Merkle tree input file generator script
